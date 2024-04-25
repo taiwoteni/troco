79a9fe873dfcff4b086808fdf406d9d32ca220f1
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-04-25 16:48:21 +0100

    Added form

---
+++ lib/features/transactions/presentation/view-transaction/widgets/upload-driver-details-form.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UploadDriverDetailsForm extends ConsumerStatefulWidget {
+  const UploadDriverDetailsForm({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _UploadDriverDetailsFormState();
+}
+
+class _UploadDriverDetailsFormState extends ConsumerState<UploadDriverDetailsForm> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
