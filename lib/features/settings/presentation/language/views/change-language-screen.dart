539b5463e9fd19e575996465481b036ddd41614f
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-05-04 13:29:13 +0100

    created change language screen

---
+++ lib/features/settings/presentation/language/views/change-language-screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ChangeLanguageScreen extends ConsumerStatefulWidget {
+  const ChangeLanguageScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ChangeLanguageScreenState();
+}
+
+class _ChangeLanguageScreenState extends ConsumerState<ChangeLanguageScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
