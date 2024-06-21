d18e9eac819f65dfde9d8090b04fd5c2676bcc6e
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-06-21 20:55:02 +0100

    Added webp extension options

--- lib/core/app/asset-manager.dart
+++ lib/core/app/asset-manager.dart
@@ -1,4 +1,4 @@
-enum Extension{png,jpeg,jpg}
+enum Extension{png,jpeg,jpg,webp}
 class AssetManager{
 
   /// [iconFile] is used for getting the a particular icon from the asset directory. 
@@ -13,7 +13,7 @@ class AssetManager{
 
   /// [imageFile] is used for getting the a particular image from the asset directory. 
   static String imageFile({required final String name, final Extension ext = Extension.png}){
-    return "assets/images/$name.${ext == Extension.png? "png":ext==Extension.jpg?"jpg": "jpeg"}";
+    return "assets/images/$name.${ext.name}";
   }
 
   /// [lottieFile] is used for getting the a particular lottie from the asset directory. 
